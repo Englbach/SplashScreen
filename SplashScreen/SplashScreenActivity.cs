f58900f7323b9e2296d54261a5299ffa62340a5b
Englbach <lethien.hoang@yahoo.com>
2016-06-20 15:45:24 +0700

    892

---
+++ SplashScreen/SplashScreenActivity.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace SplashScreen
+{
+	[Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+	public class SplashScreenActivity : Activity
+	{
+		static readonly string TAG = "X:" + typeof(SplashScreenActivity).Name;
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+		}
+		protected override void OnResume()
+		{
+			base.OnResume();
+
+			Task startupWork = new Task(() =>
+			{
+				Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+				Task.Delay(1000000);  // Simulate a bit of startup work.
+				Log.Debug(TAG, "Working in the background - important stuff.");
+			});
+
+			startupWork.ContinueWith(t =>
+			{
+				Log.Debug(TAG, "Work is finished - start Activity1.");
+				StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+			}, TaskScheduler.FromCurrentSynchronizationContext());
+
+			startupWork.Start();
+		}
+	}
+}
+
